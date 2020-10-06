0b6452f4ac1a3984612a931c009620c914718f3a
Merve <m.merve.unlu@gmail.com>
2020-10-06 21:55:13 +0300

    side bar added, to do create dynamic sidebar

--- run_app.py
+++ run_app.py
@@ -1,24 +1,41 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+
+lectures = {'lecture1':{"ylink":"https://youtu.be/eh2yO5YtqRM","subtitle":"static/ENG_001_20151602_01204_ch01_0.en.vtt"},
+            'lecture2':{"ylink":"https://youtu.be/nZAvdNK0d74","subtitle":"static/ENG_001_20151602_01204_ch01_1.en.vtt/"}}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    with open('subs/Lecture1-English.txt', 'r') as f:
-        text = f.read()
-    return render_template('video-question-page.html', text=text)
+    ylink = "https://youtu.be/eh2yO5YtqRM"
+    subtitle = "static/ENG_001_20151602_01204_ch01_0.en.vtt"
+    return render_template('video-question-page.html', ylink=ylink, subtitle=subtitle)
 
 @app.route('/question', methods=['GET', 'POST'])
 def question():
-    # TODO: altyazi text i icinde html kod olursa acaba sayfada gorunur mu?
-    # Boylece highlight kismini yapabilir miyim? 
-    with open('subs/Lecture1-English-answer.txt', 'r') as f:
-        text = f.read()
+    ylink = "https://youtu.be/eh2yO5YtqRM"
+    subtitle = "static/ENG_001_20151602_01204_ch01_0.en.vtt"
+    for lecture in lectures.keys():
+        if request.form.get(lecture):
+            # print("Button %s is clicked" %lecture)
+            ylink = lectures[lecture]["ylink"]
+            subtitle = lectures[lecture]["subtitle"]
+    return render_template('video-question-page.html', ylink=ylink, subtitle=subtitle)
+
+
+@app.route('/answer', methods=['GET', 'POST'])
+def answer():
     # TODO: answer will come from QA-server
+    ylink = request.form['ylink']
+    subtitle = request.form['subtitle']
     answer_text = "NLP people call a large pile of text a corpus"    
     return render_template('video-answer-page.html',
-                           text=text,
                            question=request.form['question'],
-                           answer=answer_text)
+                           answer=answer_text,
+                           ylink = ylink,
+                           subtitle= subtitle)
 
 
 if __name__ == "__main__":
