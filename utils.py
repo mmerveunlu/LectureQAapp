fdd7eeb6778e9dfe18d02cb0f0efb429603bc39c
Merve <m.merve.unlu@gmail.com>
2020-10-15 21:25:10 +0300

    functions not running as service, one-time-service

---
+++ utils.py
@@ -0,0 +1,56 @@
+import json
+from datetime import datetime
+import statics
+
+
+def get_questions(chapter_name, qpath):
+    """ 
+    returns previously asked questions for a given chapter
+    Args:
+     chapter_name: string, the key of the chapter name
+      ex: ch01_0_0
+     qpath: string, the path of the data file, contains questions 
+            for each chapter 
+    Returns: 
+     list of string, questions text
+    """
+    with open(qpath) as fp:
+        data = json.load(fp)
+    return data[chapter_name]
+
+def save_asked_questions(chapter,question,username,userID,qpath):
+    """
+    saves asked questions into a file 
+    Args:
+      chapter: string, the key of the chapter
+      question: string, the question text asked by user
+      username: string, the name and surname of the user 
+      userID: string, the student id of the user 
+      qpath: string, the path of the question file
+    """
+    with open(qpath,"a+") as fp:
+        line = statics.SEP.join([datetime.now().strftime(format="%d:%m:%Y-%H:%M:%S"),
+                        question,username,userID,"\n"])
+        fp.write(line)
+
+def save_questions(datapath):
+    """save the example questions into a file
+    Args: 
+      datapath: string, the path of the json file
+    Returns: 
+      None,
+    """
+    # open json file
+    with open(datapath) as fp:
+        data = json.load(fp)['data']
+    # for each chapter key find questions
+    chapter_dict = {key:[] for key in statics.chapter_keys}
+    for a in data: 
+        for p in a['paragraphs']: 
+            for qa in p['qas']: 
+                chapter_dict[qa['id'][1:8]].append(qa['question']) 
+    with open("example-questions.json","w") as fp:
+        json.dump(chapter_dict,fp,indent=4,sort_keys=True)
+    
+
+
