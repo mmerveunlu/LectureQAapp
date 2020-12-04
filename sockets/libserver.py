e1e27dee2e5fd208476e7e88fa500bb06a25d8ba
Merve Unlu <m.merve.unlu@gmail.com>
2020-12-04 11:55:15 +0300

    unnecessary search dict removed

--- sockets/libserver.py
+++ sockets/libserver.py
@@ -6,14 +6,6 @@ import struct
 from datetime import datetime
 from utils import run_model
 
-request_search = {
-    "morpheus": "Follow the white rabbit. \U0001f430",
-    "ring": "In the caves beneath the Misty Mountains. \U0001f48d",
-    "\U0001f436": "\U0001f43e Playing ball! \U0001f3d0",
-}
-
-
-
 def request_search(passage,question):
     """runs the model for given passage and question
     Args: 
