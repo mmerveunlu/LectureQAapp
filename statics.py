874292bd3a5c86b698cc861dd88d52154b4ad040
Merve <m.merve.unlu@gmail.com>
2021-02-14 19:40:45 +0300

    title added to dict

--- statics.py
+++ statics.py
@@ -13,10 +13,16 @@ chapter_keys = ['_ch03_2','_ch07_1','_ch05_1','_ch09_2','_ch01_5',
                 '_ch01_3','_ch02_2','_ch03_1','_ch03_4','_ch01_4',
                 '_ch04_1','_ch09_1','_ch01_0','_ch01_1','_ch01_2']
 
-lectures = {
-    'lecture1': {"ylink": "https://youtu.be/eh2yO5YtqRM", "subtitle": "static/ENG_001_20151602_01204_ch01_0.en.vtt", "key":"_ch01_0"},
-    'lecture2': {"ylink": "https://youtu.be/nZAvdNK0d74", "subtitle": "static/ENG_001_20151602_01204_ch01_1.en.vtt", "key":"_ch01_1"}}
 
+lectures = {
+    'lecture1': {"ylink": "https://youtu.be/eh2yO5YtqRM",
+                 "subtitle": "static/ENG_001_20151602_01204_ch01_0.en.vtt",
+                 "key":"_ch01_0",
+                 "title":"Introduction to Signal and Systems"},
+    'lecture2': {"ylink": "https://youtu.be/nZAvdNK0d74",
+                 "subtitle": "static/ENG_001_20151602_01204_ch01_1.en.vtt",
+                 "key":"_ch01_1",
+                 "title":"Transformation of the Independent Variable"}}
 SEP=","
 
 HOST="10.2.1.55"
